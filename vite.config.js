13a4a237e6692c4035dfdfb308eac202d46b1864
Your Name <byamugishanthony@gmail.com>
2025-10-11 12:12:28 +0300

    Remove github deployment setting;

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/anthony.dev/',
-  publicDir: 'public',
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split('.').pop();
-          if (/png|jpe?g|gif|svg|webp|avif|tiff/i.test(extType)) {
-            extType = 'images';
-          }
-          return `assets/${extType}/[name]-[hash][extname]`;
-        }
-      }
-    }
-  }
+  publicDir: 'public'
 })
